a4f067b051dc924b51225e22ff1505a0275a384e
Renato Cumbal <rcumbal@gmail.com>
2022-08-26 00:58:08 -0500

    Agregamos archivos
    Agregamos los archivos segundo y tercero

---
+++ segundo.js
@@ -0,0 +1,2 @@
+let a=10, b=5;
+console.log('La suma de ',a,' y ',b,' es:',a+b);
---
+++ tercero.js
@@ -0,0 +1,6 @@
+function suma(a,b){
+    let suma;
+    suma=a+b;
+    return suma
+}
+console.log('La suma es:',suma(7,8));
